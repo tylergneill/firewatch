a85edd6e49431d820e3ae28b2ad0b7813ec3eda6
Tyler Neill <tyler.g.neill@gmail.com>
2025-12-06 15:26:11 -0500

    feat: split refresh script into both up and down

---
+++ utils/refresh_data_up.sh
@@ -0,0 +1,7 @@
+LOCAL_DATA_PATH="../firewatch-data"
+for suffix in "" "-stg"; do
+    for appname in skrutable splitter-server vatayana panditya hansel firewatch kalpataru-grove; do
+        rsync -avz -e "ssh -i ~/.ssh/id_rsa_do" --size-only --delete ${LOCAL_DATA_PATH}/${appname}${suffix}-archive/ root@146.190.74.47:/var/log/nginx/${appname}${suffix}-archive/
+        rsync -avz -e "ssh -i ~/.ssh/id_rsa_do" ${LOCAL_DATA_PATH}/${appname}${suffix}-app.access.log root@146.190.74.47:/var/log/nginx/${appname}${suffix}-app.access.log
+    done
+done
